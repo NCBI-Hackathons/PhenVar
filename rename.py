03f412af3b2fb17d8838fb05c1540319edc29b42
Ubuntu <ubuntu@ip-172-16-241-228.ec2.internal>
2017-01-09 18:22:06 +0000

    grabbing abstracts and just printing them

--- rename.py
+++ rename.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python2
 
 from Bio import Entrez
+import time # We need this if we want to filter based on age
+from urllib2 import HTTPError
+
+# Configuratoin settings to be moved externally later
+email = "jon.demasi@colorado.edu"
 
 
 """ 
@@ -8,17 +13,27 @@ Take a search term, intended to be an rs#,
 and return a list of pmids
 """
 def get_pmids(interm):
-    print("This is doing stuff")
-    Entrez.email = "jon.demasi@colorado.edu"     # Always tell NCBI who you are
-    handle = Entrez.esearch(db="pubmed", term=interm)
-    record = Entrez.read(handle)
+    #print("This is doing stuff")
+    Entrez.email = email     # Always tell NCBI who you are
+    search_results = Entrez.read(Entrez.esearch(db="pubmed", term=interm, usehistory="y"))
     #print(record)
     #print(record["IdList"])
-    return(record["IdList"])
+    return search_results
 
 
-def get_abstract(pmid):
+def get_abstracts(results):
+    Entrez.email = email 
+    #handle = Entrez.efetch(db="pubmed", id="26999119", rettype="abstract", retmode="text")
+    #print(handle.readline().strip())
+    #handle.close()
+    fetch_handle = Entrez.efetch(db="pubmed",rettype="abstract",retmode="text",webenv=results["WebEnv"],query_key=results["QueryKey"])
+    data=fetch_handle.read()
+    print(data)
+    fetch_handle.close()
     return
+
 # Just using this to test get_pmids function
-list = get_pmids("rs328")
-print(list)
+
+toquery=get_pmids("rs328")
+get_abstracts(toquery)
+
