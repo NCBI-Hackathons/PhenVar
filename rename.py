d96a67015b458b22cc12423b86f0617c5429334f
Ubuntu <ubuntu@ip-172-16-241-228.ec2.internal>
2017-01-09 17:00:41 +0000

    finished initial function and added skeleton for second

--- rename.py
+++ rename.py
@@ -2,13 +2,23 @@
 
 from Bio import Entrez
 
-def get_from_search_term(interm):
+
+""" 
+Take a search term, intended to be an rs#, 
+and return a list of pmids
+"""
+def get_pmids(interm):
     print("This is doing stuff")
     Entrez.email = "jon.demasi@colorado.edu"     # Always tell NCBI who you are
     handle = Entrez.esearch(db="pubmed", term=interm)
     record = Entrez.read(handle)
-    print(record)
-    print(record["IdList"])
-    return
+    #print(record)
+    #print(record["IdList"])
+    return(record["IdList"])
 
-get_from_search_term("rs328")
+
+def get_abstract(pmid):
+    return
+# Just using this to test get_pmids function
+list = get_pmids("rs328")
+print(list)
