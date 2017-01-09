1de9d3d4f4f4dfa4aaf1b061ed4bea3a9d08259d
Ubuntu <ubuntu@ip-172-16-241-228.ec2.internal>
2017-01-09 18:53:47 +0000

    parsing out abstracts

--- rename.py
+++ rename.py
@@ -26,14 +26,25 @@ def get_abstracts(results):
     #handle = Entrez.efetch(db="pubmed", id="26999119", rettype="abstract", retmode="text")
     #print(handle.readline().strip())
     #handle.close()
-    fetch_handle = Entrez.efetch(db="pubmed",rettype="abstract",retmode="text",webenv=results["WebEnv"],query_key=results["QueryKey"])
-    data=fetch_handle.read()
-    print(data)
-    fetch_handle.close()
-    return
+    #fetch_handle = Entrez.efetch(db="pubmed",rettype="abstract",retmode="text",webenv=results["WebEnv"],query_key=results["QueryKey"])
+    #data=fetch_handle.read()
+    #print(data)
+    #fetch_handle.close()
+    for start in range(0,int(results["Count"]),1):
+        end = min(int(results["Count"]), start+1)
+        print("Going to download record %i to %i" % (start+1, end))
+        fetch_handle = Entrez.efetch(db="pubmed",rettype="abstract",
+                                         retmode="text",retstart=start,
+                                         retmax=1,
+                                         webenv=results["WebEnv"],
+                                         query_key=results["QueryKey"])
+        data = fetch_handle.read()
+        fetch_handle.close()
+        print(data)
 
 # Just using this to test get_pmids function
 
 toquery=get_pmids("rs328")
+print(toquery["Count"])
 get_abstracts(toquery)
 
