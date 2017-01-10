e1f32736e2ec47e6794835b82c79f47e514004ed
Ashika-Sita <jayanthyashika@gmail.com>
2017-01-10 16:31:31 -0500

    test script for wordcloud added

---
+++ test_wordcloudfornouns.py
@@ -0,0 +1,27 @@
+from lanpros import *
+from wordcloud import *
+import matplotlib.pyplot as plt
+from ncbiutils import *
+
+def create_wordcloud(normalized_all_counts):
+
+    word_cloud_list = [(key + ' ') * int(round(normalized_all_counts[key],4)*10000) for key in normalized_all_counts.keys()]
+    word_cloud_text = '-'.join(word_cloud_list)
+    word_cloud = WordCloud().generate(word_cloud_text)
+    plt.imshow(word_cloud)
+    plt.axis("off")
+    plt.savefig("figure.png")
+    plt.show()
+    return
+
+
+#test implementation
+
+toquery=get_pmids("rs328")
+
+abstracts = get_abstracts(toquery)
+
+t = tokenize_abstracts(abstracts)
+q = tagged_abstracts(t)
+n = extract_nouns(q)
+create_wordcloud(n)
