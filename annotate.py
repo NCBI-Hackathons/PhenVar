8bc3f999ad7d440536b538c680f6becbd7653029
Ubuntu <ubuntu@ip-172-16-241-228.ec2.internal>
2017-01-10 15:00:05 +0000

    clean script to just keep the function to get annotation

--- annotate.py
+++ annotate.py
@@ -1,30 +1,15 @@
 #!/usr/bin/env python2
 
 from Bio import Entrez
-import time # We need this if we want to filter based on age
+import time  # We need this if we want to filter based on age
 from urllib2 import HTTPError
 import xml.etree.ElementTree as ET
-import argparse
-import csv
+from settings import configuration
+email = configuration["email"]
 
-def parse_args():
-    """
-    Parse command-line arguments
-    """
-    parser = argparse.ArgumentParser(description="This script grabs information from esummary. Currently this script will grab GLOBAL_MAF, CLINICAL_SIGNIFICANCE, CHR, FXN_CLASS")
-
-    parser.add_argument(
-            "--RS_ID", required=True,
-            help="REQUIRED. RS ID of interest")
-    parser.add_argument(
-            "--email_address", required=True,
-            help="REQUIRED. Email address is needed to call eutils")
-    parser.add_argument("--outfile", required=True, 
-            help="REQUIRED. Name of output file.")
-    args = parser.parse_args()
-    return args
 
 def annotate_RS_ID(RS_ID, email):
+    """This script should return annotation asspciate with each RS ID. Can incorporate more information later"""
     Entrez.email = email
     handle = Entrez.esummary(db="snp", id=RS_ID)
     record = handle.read()
@@ -43,15 +28,5 @@ def annotate_RS_ID(RS_ID, email):
         if attribute['Name'] == 'FXN_CLASS':
             result.append(item.text)
     return result
-
-def main():
-    args = parse_args()
-    RS_ID = str(args.RS_ID)
-    email = str(args.email_address)
-    result = annotate_RS_ID(RS_ID, email)
-    with open(args.outfile,"w") as f:
-        w = csv.writer(f)
-        w.writerows(result)
-main()
-
+# TODO: add header to output file
 
