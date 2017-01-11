f5a983161d37713b417c2a4deae2089beaf025d0
Jonathan DeMasi <jode5573@colorado.edu>
2017-01-11 14:54:39 -0500

    pep8

--- wordcloudfornouns.py
+++ wordcloudfornouns.py
@@ -4,9 +4,11 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
+
 def create_wordcloud(normalized_all_counts, outfig):
 
-    word_cloud_list = [(key + ' ') * int(round(normalized_all_counts[key],4)*10000) for key in normalized_all_counts.keys()]
+    word_cloud_list = [(key + ' ') * int(round(normalized_all_counts[key], 4) * 10000)
+                       for key in normalized_all_counts.keys()]
     word_cloud_text = '-'.join(word_cloud_list)
     word_cloud = WordCloud().generate(word_cloud_text)
     fig = plt.imshow(word_cloud)
