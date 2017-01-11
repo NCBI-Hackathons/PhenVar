9f15e83906905f4e473b562eb9863000cd15ae72
Ubuntu <ubuntu@ip-172-16-241-228.ec2.internal>
2017-01-11 17:44:39 +0000

    working version for validating

--- wordcloudfornouns.py
+++ wordcloudfornouns.py
@@ -5,17 +5,16 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-def create_wordcloud(normalized_all_counts):
+def create_wordcloud(normalized_all_counts, outfig):
 
     word_cloud_list = [(key + ' ') * int(round(normalized_all_counts[key],4)*10000) for key in normalized_all_counts.keys()]
     word_cloud_text = '-'.join(word_cloud_list)
     word_cloud = WordCloud().generate(word_cloud_text)
     fig = plt.imshow(word_cloud)
     plt.axis("off")
-<<<<<<< HEAD
-    plt.savefig("figure.png")
+
+#    plt.savefig("figure.png")
 #    plt.show()
-=======
-    plt.savefig("wordcloud.png")
->>>>>>> c2e0c9a6ac820f9aae06389f91ef9a168920d502
+    # plt.savefig("/home/ubuntu/git_repos/Phenotypic_Clustering_of_Variants/plots/rs328_wordcloud_prefilter.png")
+    plt.savefig(outfig)
     return
