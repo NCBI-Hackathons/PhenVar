bbce2a82bee00c14b26e1122c34b48aab1677e8e
Cassidy Thompson <cassidy.thompson@colorado.edu>
2017-01-31 16:20:18 -0700

    Adding basic main template

--- phenvar.py
+++ phenvar.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-from flask import Flask
+from flask import Flask, render_template
 application = Flask(__name__)
 
 
 @application.route("/")
 def index():
-    return "PhenVar!"
+    return render_template('index.html', name=index)
 
 if __name__ == "main":
     application.run()
