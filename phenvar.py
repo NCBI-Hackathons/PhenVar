da4dd2cd301ef76542e7b6b1175fd2aa2c75373b
Cassidy Thompson <cassidy.thompson@colorado.edu>
2017-01-31 18:14:24 -0700

    Added basic wordcloud functionality using d3-based wordcloud generator

--- phenvar.py
+++ phenvar.py
@@ -1,11 +1,26 @@
 #!/usr/bin/env python3
-from flask import Flask, render_template
+from flask import Flask, request, render_template
+from visualization import word_blob
 application = Flask(__name__)
 
 
 @application.route("/")
 def index():
-    return render_template('index.html', name=index)
+    return render_template('index.html')
+
+@application.route("/results/", methods=["GET", "POST"])
+def results():
+    rsid_string = request.form["rsids"]
+    rsid_list = rsid_string.split()
+    wordcloud_text = word_blob(rsid_list)
+    return render_template('wordcloud.html', wordcloud_text=wordcloud_text)
+
+
+@application.route("/test")
+def test():
+    test_rsids = [328]
+    blob = word_blob(test_rsids)
+    return render_template('wordcloud.html', wordcloud_text=blob)
 
 if __name__ == "main":
     application.run()
--- visualization.py
+++ visualization.py
@@ -3,6 +3,16 @@ import matplotlib.pyplot as plt
 from database import session, RSIDCitation, Article
 
 
+def word_blob(rsid_list):
+    noun_list = []
+    pmid_tuples = session.query(RSIDCitation.pmid).filter(RSIDCitation.rsid.in_(rsid_list)).all()
+    pmid_list = [pmid_tuple[0] for pmid_tuple in pmid_tuples]
+    articles = session.query(Article).filter(Article.pmid.in_(pmid_list)).all()
+    for article in articles:
+        noun_list += article.abstract_nouns()
+    return " ".join(noun_list)
+
+
 # Generate a wordcloud png file from a list of rsids
 def wordcloud_from_rsids(rsid_list, output_path=None):
     noun_list = []
