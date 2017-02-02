f2946f7e157e96f671e35ba61178552aef442822
Cassidy Thompson <cassidy.thompson@colorado.edu>
2017-02-02 13:29:18 -0700

    Added error handling in event of no rsid citations found.

--- visualization.py
+++ visualization.py
@@ -1,15 +1,15 @@
 from wordcloud import *
-import matplotlib.pyplot as plt
 from database import session, RSIDCitation, Article
-import hashlib
 from settings import WORDCLOUD_STORAGE
+import matplotlib.pyplot as plt
+import hashlib
 import os
 
 
 def word_blob(rsid_list):
     noun_list = []
-    pmid_tuples = session.query(RSIDCitation.pmid).filter(RSIDCitation.rsid.in_(rsid_list)).all()
-    pmid_list = [pmid_tuple[0] for pmid_tuple in pmid_tuples]
+    pmid_list = [pmid_tuple[0] for pmid_tuple
+                 in session.query(RSIDCitation.pmid).filter(RSIDCitation.rsid.in_(rsid_list)).all()]
     articles = session.query(Article).filter(Article.pmid.in_(pmid_list)).all()
     for article in articles:
         noun_list += article.abstract_nouns()
@@ -17,33 +17,25 @@ def word_blob(rsid_list):
 
 
 # Generate a wordcloud png file from a list of rsids
-def wordcloud_from_rsids(rsid_list, output_path=None):
-    noun_list = []
-    pmid_tuples = session.query(RSIDCitation.pmid).filter(RSIDCitation.rsid.in_(rsid_list)).all()
-    pmid_list = [pmid_tuple[0] for pmid_tuple in pmid_tuples]
-    articles = session.query(Article).filter(Article.pmid.in_(pmid_list)).all()
-    for article in articles:
-        noun_list += article.abstract_nouns()
-    full_text = " ".join(noun_list)
+def create_wordcloud_from_rsids(rsid_list, output_path=None):
+    full_text = word_blob(rsid_list)
     word_cloud = WordCloud(width=1600, height=800).generate(full_text)
-    if output_path is not None:
-        #plt.figure(figsize=(20, 10), facecolor='k')
-        ##plt.tight_layout(pad=0)
-        #plt.imshow(word_cloud)
-        #plt.axis("off")
-        plt.figure(figsize=(20, 10), facecolor='k')
-        plt.imshow(word_cloud)
-        plt.axis("off")
-        plt.tight_layout(pad=0)
-        plt.show()
-        plt.savefig(output_path, facecolor='k', bbox_inches='tight')
-    return word_cloud
+    plt.figure(figsize=(20, 10), facecolor='k')
+    plt.imshow(word_cloud)
+    plt.axis("off")
+    plt.tight_layout(pad=0)
+    plt.show()
+    plt.savefig(output_path, facecolor='k', bbox_inches='tight')
 
 
+# Function results in IndexError if no results are found
 def generate_wordcloud(rsid_list):
     rsid_string = " ".join(rsid_list)
     hash_object = hashlib.sha1(rsid_string.encode())
     hashed_file_name = "{}.png".format(hash_object.hexdigest())
     hashed_file_path = os.path.join(WORDCLOUD_STORAGE, hashed_file_name)
-    wordcloud_from_rsids(rsid_list, hashed_file_path)
-    return hashed_file_name
+    try:
+        create_wordcloud_from_rsids(rsid_list, hashed_file_path)
+        return hashed_file_name
+    except IndexError:
+        return ""
