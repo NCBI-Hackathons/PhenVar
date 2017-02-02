34c6255610f21f1afd55dc6bd569ed037e39113d
Cassidy Thompson <cassdamthompson@gmail.com>
2017-02-02 15:03:05 -0700

    huh

--- database.py
+++ database.py
@@ -31,10 +31,10 @@ class Article(Base):
         nouns = []
         tokens = nltk.word_tokenize(self.abstract)
         tagged = nltk.pos_tag(tokens)
-        for word in [tagged_word[0].lower() for tagged_word in tagged if tagged_word[1] in ("NN", "NNS", "NNP", "NNPS")
+        nouns = [tagged_word[0].lower() for tagged_word in tagged if tagged_word[1] in ("NN", "NNS", "NNP", "NNPS")
                      and tagged_word[0].lower() not in FILTER_LIST
-                     and len(tagged_word[0]) > 1]:
-            nouns.append(word)
+                     and len(tagged_word[0]) > 1]
+        #    nouns.append(word)
         #for tagged_word in tagged:
         #    if tagged_word[1] in ("NN", "NNS", "NNP", "NNPS") and tagged_word[0] not in filter_list and len(tagged_word[0]) != 1:
         #        nouns.append(tagged_word[0])
