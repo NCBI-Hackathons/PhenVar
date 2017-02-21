2fe78fac7d4dae2497556f12131fa58dd43c3c56
Cassidy Thompson <cassidy.thompson@colorado.edu>
2017-02-21 12:03:48 -0700

    Added ability to select between three different normalization types:  - default: normal, just adds up all nouns from distinct articles associated with requested rsids  - by rsid: divides word counts associated with each requested rsid by the number of articles associated with said rsid, in order to prevent one frequently cited rsid from dominating another's results  - by article: multiplies word counts by the number of articles each word shows up in, to give greater weight to words that are mentioned frequently in many articles.

--- phenvar.py
+++ phenvar.py
@@ -13,7 +13,8 @@ def index():
 def results():
     rsid_string = request.form["rsids"].strip("rs")
     rsid_list = rsid_string.split()
-    wordcloud_file_name = generate_wordcloud(rsid_list)
+    normalization_type = request.form["normalization_type"]
+    wordcloud_file_name = generate_wordcloud(rsid_list, normalization_type)
     return render_template('results.html', wordcloud_file_name=wordcloud_file_name)
 
 @application.route("/about/")
