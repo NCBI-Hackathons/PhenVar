fb7ca6114991ab545079e8d81982e74186454484
Cassidy Thompson <cassidy.thompson@colorado.edu>
2017-02-22 13:19:35 -0700

    Added markdown files for use in templates via Flask-Markdown package

---
+++ markdown/home.md
@@ -0,0 +1,4 @@
+
+PhenVar is designed to take one or more rsids and generate a list of PubMed IDs to query and generate novel associations between publications. It utilizes a local sqlite cache in a configurable location to keep a local copy of relevant srids, pmids, and the abstract blobs for the pmids.
+
+The source code for PhenVar is available at [GitHub](https://github.com/NCBI-Hackathons/PhenVar).
--- phenvar.py
+++ phenvar.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 from flask import Flask, request, render_template
+from flaskext.markdown import Markdown
 from visualization import generate_wordcloud
 application = Flask(__name__)
+Markdown(application)
 
 
 @application.route("/")
 def index():
-    return render_template('index.html')
+    home_markdown = open('markdown/home.md', 'r').read()
+    return render_template('index.html', home_markdown=home_markdown)
 
 
 @application.route("/results/", methods=["GET", "POST"])
@@ -19,7 +22,8 @@ def results():
 
 @application.route("/about/")
 def about():
-    return render_template('about.html')
+    about_markdown = open('markdown/about.md', 'r').read()
+    return render_template('about.html', about_markdown=about_markdown)
 
 if __name__ == "main":
     application.run()
