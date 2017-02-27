f3fdb44c5cb21a6a4684b8e2c0989282355ece2c
Cassidy Thompson <cassdamthompson@gmail.com>
2017-02-27 15:55:21 -0700

    Added basic logging of ip addresses to logs/visits.log

---
+++ logs/README.md
@@ -0,0 +1,2 @@
+This directory is used to hold log files containing information we want to keep about site visits.
+This will hold a file visits.log, which will contain <date>\t<ip address> information from visitors who use the tool to generate wordclouds.
--- phenvar.py
+++ phenvar.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from flask import Flask, request, render_template
+from datetime import datetime
 from visualization import generate_wordcloud
 application = Flask(__name__)
 
@@ -11,6 +12,10 @@ def index():
 
 @application.route("/results/", methods=["GET", "POST"])
 def results():
+    # Log user's IP address
+    with open("logs/visits.log", "a") as logfile:
+        log_string = "{}\t{}\n".format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), request.environ["REMOTE_ADDR"])
+        logfile.write(log_string)
     rsid_string = request.form["rsids"].strip("rs")
     rsid_list = rsid_string.split()
     wordcloud_file_name = generate_wordcloud(rsid_list)
